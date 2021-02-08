207fb9166863046eb633cdb73fee13f9b00b5bd8
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-02-08 20:29:37 +0900

    문제 풀이 test

---
+++ algorithm/src/codeUp/Basic1010.java
@@ -0,0 +1,12 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Basic1010 {
+	public static void main() {
+		int n;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		System.out.print(n);
+	}
+}
