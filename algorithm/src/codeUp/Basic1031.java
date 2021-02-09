29fc396768a9a806a05ebbedc8eb84e022f9accf
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-02-10 00:21:47 +0900

    10진 정수 입력 받아 8진수로 출력

---
+++ algorithm/src/codeUp/Basic1031.java
@@ -0,0 +1,14 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Basic1031 {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		System.out.printf("%o",n);
+	}
+
+}
