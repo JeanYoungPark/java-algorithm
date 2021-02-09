3cfa809c8a34e2cd5d48526733e08943f6616462
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-02-09 22:19:30 +0900

    실수 입력받아 둘째 자리까지 출력

---
+++ algorithm/src/codeUp/Basic1015.java
@@ -0,0 +1,14 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Basic1015 {
+
+	public static void main(String[] args) {
+		float n;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextFloat();
+		System.out.println(String.format("%.2f",n));
+	}
+
+}
