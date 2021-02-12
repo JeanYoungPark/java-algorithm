842ea5a2c50ec4020ba9378a77efbd6bce588569
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-02-12 12:44:59 +0900

    16진수 구구단

---
+++ algorithm/src/codeUp/Basic1082.java
@@ -0,0 +1,17 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Basic1082 {
+
+	public static void main(String[] args) {
+		int a;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt(16); //16진수로 입력받음
+		
+		for (int i = 0; i < 16; i++) {
+			System.out.printf("%X*%X=%X\n",a,i,a*i);
+		}	
+	}
+
+}
