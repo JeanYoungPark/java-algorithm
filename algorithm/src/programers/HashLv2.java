d89baa2a0754243310da335ca65a5ab327ea691a
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-05-07 17:59:39 +0900

    전화번호 목록

---
+++ algorithm/src/programers/HashLv2.java
@@ -0,0 +1,31 @@
+package programers;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class HashLv2 {
+    public static void main(String[] args)throws IOException{
+        String[] phone_book = {"119", "97674223", "1195524421"};
+        solution(phone_book);
+    }
+
+    static boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashMap<String, String> map = new HashMap<>();
+
+        for(String p:phone_book){
+            map.put(p,p);
+        }
+
+        for(String p:phone_book){
+            for(int i = 0; i<p.length();i++){
+                if(map.containsKey(p.substring(0,i))){
+                    answer = false;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
